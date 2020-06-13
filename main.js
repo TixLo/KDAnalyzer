5c694906f735ba12c1c9a0abd0afeb5d428c1ff4
Tix Lo <tixlo.tw@gmail.com>
2020-06-13 22:53:40 +0800

    init KDAnalyzer project with Electron

---
+++ main.js
@@ -0,0 +1,45 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+
+var menu = Menu.buildFromTemplate([
+    {
+        label: 'Menu',
+        submenu: [
+            {
+              label:'Exit',
+              click() {
+                app.exit();
+              }
+            }
+        ]
+    }
+])
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({width: 800, height: 800 })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.setTitle("KDAnalyzer")
+}
+
+app.on('ready', () => {
+    // Menu.setApplicationMenu(menu)
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    app.quit();
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
